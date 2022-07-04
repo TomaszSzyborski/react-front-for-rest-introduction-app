95c76f2936a2b560d50f1fc73e3981b4a3b91202
tszyborski <tomasz.szyborski@hpe.com>
2022-07-04 19:59:37 +0200

    reception with columns

--- src/routes/reception.jsx
+++ src/routes/reception.jsx
@@ -2,9 +2,16 @@ import axios from "axios";
 import {useEffect, useRef, useState} from "react";
 import React from "react";
 
+const greeting = "Evening, Comrade!\n" +
+    "Let me fetch the keys, while you write your name in the workbook"
+
+const alreadyAtWork = "Akimov, Aleksandr Fyodorovich\n" +
+    "Baranov, Anatoly Ivanovich\n" +
+    "Sitnikov, Anatoly Andreyevich\n" +
+    "Toptunov, Leonid Fedorovych\n"
 
 export default function Reception() {
-    const [getText, setText] = useState("")
+    const [getText, setText] = useState(greeting)
     const [getName, setName] = useState("");
     const [getKey, setKey] = useState("");
 
@@ -36,42 +43,39 @@ export default function Reception() {
     return (
         <main style={{padding: "1rem 0"}}>
             <div className={"columns has-retro-text"}>
-                <div className={"column"}>
+                <div className={"column"}></div>
+                <div className={"column is-two-thirds"}>
                     <div className={"columns"}>
-                        <div className={"column is-one-third"}></div>
-                        <div className={"column is-two-thirds has-text-centered"}>
-                            Evening, Comrade!
-                            <br/>
-                            Let me fetch the keys, while you write your name in the workbook
+                        <div className={"column"}></div>
+
+                        <div className={"column is-two-thirds"}>
+                            <div id={"message"} className={"has-text-centered"}>{getText}</div>
+                            <div className={"spacer-one-twentieth-height"}></div>
+                            <form onSubmit={registerAtDesk}>
+                                <div className="field">
+                                   <textarea
+                                       className={"textarea is-fullwidth is-large"}
+                                       placeholder={alreadyAtWork}
+                                       value={getName}
+                                       onChange={e => setName(e.target.value)}
+                                   />
+                                </div>
+                                <div className={"spacer-one-twentieth-height"}></div>
+                                <div className={"columns"}>
+                                    <div className={"column"}></div>
+                                    <button className={"button column is-two-thirds is-primary is-large"}
+                                            type="submit"
+                                            name="Register">Write in registrar book
+                                    </button>
+                                    <div className={"column"}></div>
+                                </div>
+                            </form>
+                            <div id={"tray"}>{getKey}</div>
                         </div>
+                        <div className={"column"}></div>
                     </div>
                 </div>
-                <div className={"column"}>
-                    <form onSubmit={registerAtDesk}>
-                        <div className="field">
-                            <div className={"column is-two-thirds"}>
-                            <textarea
-                                className={"textarea is-large"}
-                                placeholder={"Akimov, Aleksandr Fyodorovich\n" +
-                                    "Baranov, Anatoly Ivanovich\n" +
-                                    "Sitnikov, Anatoly Andreyevich\n" +
-                                    "Toptunov, Leonid Fedorovych\n"
-                                }
-                                value={getName}
-                                onChange={e => setName(e.target.value)}
-                            />
-                            </div>
-                            <button className={"button is-primary is-large column is-two-thirds"}
-                                    type="submit"
-                                    name="Register">Write in registrar book
-                            </button>
-                        </div>
-                    </form>
-                    <div className={"column is-two-thirds"}>
-                        <div id={"message"}>{getText}</div>
-                        <div id={"tray"}>{getKey}</div>
-                    </div>
-                </div>
+                <div className={"column"}></div>
             </div>
         </main>
     )
